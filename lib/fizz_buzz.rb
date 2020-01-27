de371baebc2747e1d39337f88d55900eaa2ee4fe
pauloswardblad <paulo@swardblads.se>
2020-01-27 22:19:03 +0100

    added divisable by 3,5,15, run test and works

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,9 +1,17 @@
 
 def fizz_buzz (number)
-    if number%3 == 0
-        "Fizz!"   
+    if has_zero_remainder?(number, 15)
+    "FizzBuzz!"   
+    elsif has_zero_remainder?(number, 5)
+    'Buzz!'  
+    elsif has_zero_remainder?(number, 3)
+    'Fizz!' 
     else
-    number    
+        number  
     end
-
 end
+
+def has_zero_remainder?(number, divider)
+    number%divider == 0
+end
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -7,7 +7,15 @@ describe 'fizz_buzz' do
     end
     
     it 'returns Fizz! if number is divisable by 3' do
-    expect(fizz_buzz(3)). to eq 'Fizz!'
+    expect(fizz_buzz(6)). to eq 'Fizz!'
+    end
+
+    it 'returns Fizz! if number is divisable by 5' do
+    expect(fizz_buzz(10)). to eq 'Buzz!'
+    end
+
+    it 'returns FizzBuzz! if number is divisable by 15' do
+    expect(fizz_buzz(30)). to eq 'FizzBuzz!'
     end
     
 end
